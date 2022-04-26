51af93f9907313b4b616dcf9c1e928b74d7abd0b
Prashant-SL <pacchidon07@gmail.com>
2022-04-26 17:58:26 +0530

    random commit

--- client/src/Components/Home.jsx
+++ client/src/Components/Home.jsx
@@ -1,14 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
 	const [data, setData] = useState([]);
-	// const getData = async (data) => {
-	// 	// const res = await axios.get('http://localhost:5000/listing');
-	// };
+	useEffect(() => {
+		getData();
+	}, []);
+	const getData = async (data) => {
+		try {
+			const res = await axios.get('http://localhost:5000/listing');
+			setData([...res.data]);
+			console.log([...res.data]);
+		} catch (e) {
+			console.log(e.message);
+		}
+	};
 	return (
 		<>
-			<table>
+			<br />
+			<br />
+			<table border="1">
 				<thead>
 					<tr>
 						<td>ID</td>
@@ -22,16 +34,22 @@ const Home = () => {
 					</tr>
 				</thead>
 				<tbody>
-					<tr>
-						<td>1</td>
-						<td>1</td>
-						<td>1</td>
-						<td>1</td>
-						<td>1</td>
-						<td>1</td>
-						<td>1</td>
-						<td>1</td>
-					</tr>
+					{data.map((el) => {
+						return (
+							<tr key={el.id}>
+								<td>
+									<Link to={`/listing/${el.id}`}>{el.id}</Link>
+								</td>
+								<td>{el.name}</td>
+								<td>{el.city}</td>
+								<td>{el.address}</td>
+								<td>{el.capacity}</td>
+								<td>{el.cost}</td>
+								<td>{el.verified}</td>
+								<td>{el.rating}</td>
+							</tr>
+						);
+					})}
 				</tbody>
 			</table>
 		</>
